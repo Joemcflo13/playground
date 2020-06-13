99ded3c4f0e34c283cba659d421d49833fa99c9e
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-06-12 20:01:41 -0600

    Added block and icon and color

---
+++ Playground.ts
@@ -0,0 +1,15 @@
+// credit for icon: https://semantic-ui.com/elements/icon.html, i wouldn't have it without it.
+//% color="#ff0000" icon="\uf1d8"
+namespace Playground {
+    //%block="what is a letter for the word? $arg"
+    //%blockId=scrambler
+    //% enumName="Letters"
+    //% enumMemberName="letter"
+    //% enumPromptHint="A, B, C ..."
+    //% enumInitialMembers="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
+    export function scrambler(arg: string)  {
+        let letter = arg;
+        return arg;
+    }
+    
+}
