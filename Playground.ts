16a34f1bf0ca9fc8ebadf7233e08169b4e750314
Joemcflo13 <65471652+Joemcflo13@users.noreply.github.com>
2020-06-12 20:25:09 -0600

    Update Playground.ts

--- Playground.ts
+++ Playground.ts
@@ -1,6 +1,9 @@
 // credit for icon: https://semantic-ui.com/elements/icon.html, i wouldn't have it without it.
 //% color="#ff0000" icon="\uf2bd"
 namespace Playground {
+    /**
+     * this block is for scrambeling letters.
+     */
     //%block="what is a letter for the word? $arg"
     //%blockId=scrambler
     //% enumName="Letters"
@@ -12,4 +15,13 @@ namespace Playground {
         return arg;
     }
     
+    /**
+     * this block is what you use to make words with the scrambler.
+     */
+    //%block="show randomized string: $message | $message2 | $message3"
+    //%blockId=randomscrambler
+    export function randomizer(message: string, message2: string, message3: string,)  {
+        basic.showString("message, message2, message3,")
+    }
+
 }
